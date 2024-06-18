91e4e0ae22b8d1f1d4bbb3a4b5b7581b99a4dfba
NadiaXY <gbnadia025@gmail.com>
2024-06-18 23:48:35 +0200

    Initial Commit

---
+++ Swift test/Swift_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_testApp.swift
+//  Swift test
+//
+//  Created by Nadia Marina Gaspar Baptista on 18.06.24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

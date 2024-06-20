f5f5d01c161f5c8f933dbdc62df0fc39985f3b57
NadiaXY <gbnadia025@gmail.com>
2024-06-20 08:03:12 +0200

    update

--- Swift test/ContentView.swift
+++ Swift test/ContentView.swift
@@ -8,19 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var birthdate = Date()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, my world!")
+        Form {
+            DatePicker("Birthdate", selection: $birthdate)
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
 
 
